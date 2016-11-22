b02f70ddabb7f786a83c714364669892cc83420c
Chris Usick <chris.usick@nfl.com>
2016-11-22 06:28:43 -0600

    added id_token utility

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/auth/mobile', to: 'mobile_auth#sign_in'
+  get '/dev/auth_token', file: 'index.html'
 end
